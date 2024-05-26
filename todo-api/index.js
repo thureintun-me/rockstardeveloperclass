13f92116bdcf7f02545fb467bb040489fca93c28
thurein <thureintun@acedatasystems.com>
2024-05-26 15:39:25 +0630

    add update by id and toggle

--- todo-api/index.js
+++ todo-api/index.js
@@ -14,7 +14,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/tasks", async function (req, res) {
   const data = await db.collection("tasks").find().toArray();
-  console.log("Data", data);
   return res.json(data);
 });
 
@@ -34,7 +33,7 @@ app.delete("/tasks/:id", async function (req, res) {
 
 app.post("/tasks", async (req, res) => {
   const { name } = req.body;
-  console.log("name", name);
+
   if (!name) return res.status(400).json({ msg: "name required" });
 
   const result = await db.collection("tasks").insertOne({
@@ -47,6 +46,49 @@ app.post("/tasks", async (req, res) => {
     id: result.insertedId,
     done: false,
   });
+});
+
+app.put("/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  if (!name) return res.status(400).json({ msg: "name required" });
+
+  try {
+    const result = await db.collection("tasks").updateOne(
+      {
+        _id: new ObjectId(id),
+      },
+
+      {
+        $set: {
+          name,
+        },
+      }
+    );
+
+    return res.json(result);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+app.put("/tasks/:id/toggle", async (req, res) => {
+  const { id } = req.params;
+  const data = await db.collection("tasks").findOne({ _id: new ObjectId(id) });
+  const result = await db.collection("tasks").updateOne(
+    {
+      _id: new ObjectId(id),
+    },
+
+    {
+      $set: {
+        done: !data.done,
+      },
+    }
+  );
+
+  return res.json(result);
 });
 
 app.listen("8080", () => {
